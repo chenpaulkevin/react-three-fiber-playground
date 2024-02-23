e7094965cf7a18ed120b7f6334b91fa73667454f
Paul Chen <47184835+chenpaulkevin@users.noreply.github.com>
2024-02-23 18:00:55 +0800

    Material Color, Hover Interaction, directionalLighting

--- app/components/cube/cube.tsx
+++ app/components/cube/cube.tsx
@@ -1,16 +1,14 @@
 "use client";
 import React, { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { PerspectiveCamera } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { motion } from "framer-motion-3d";
 
 export default function Cube() {
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <Canvas>
-        <PerspectiveCamera position={[0, 0, 10]} makeDefault>
-          {" "}
-        </PerspectiveCamera>
+        <OrbitControls enableZoom={false} enablePan={false} />
         <ambientLight intensity={1}></ambientLight>
         <directionalLight intensity={5} position={[2, 1, 1]} />
         <Object />
@@ -21,15 +19,19 @@ export default function Cube() {
 
 function Object() {
   const mesh = useRef(null);
+  const [color, setColor] = useState("pink");
+  const [scale, setScale] = useState(false);
+  const [height, setHeight] = useState(1);
+  useFrame((state, delta) => {
+    // @ts-ignore
+    mesh.current.rotation.x += delta * 0.4;
+    // @ts-ignore
+    mesh.current.rotation.y += delta * 0.4;
+    // @ts-ignore
+    mesh.current.rotation.z += delta * 0.4;
+  });
   return (
-    <motion.mesh
-      ref={mesh}
-      position={[0, 0, 0]}
-      whileHover={{
-        scaleX: 3,
-        transition: { duration: 0.2, ease: "easeOut" },
-      }}
-    >
+    <motion.mesh ref={mesh} position={[0, 0, 0]} whileHover={{ scaleX: 3 }}>
       <boxGeometry args={[1, 1, 1]} />
       <motion.meshStandardMaterial
         initial={{ color: "blue" }}
